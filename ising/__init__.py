e64e9717d6377338d1ce914eda78e2e9e76423bc
Daniel Karandikar <Daniel Karandikar>
2017-01-26 21:21:29 +0000

    Magnetization graphs
    Introduced magnetization graph - this is only really succesful when running standard() rather than test()

--- ising/__init__.py
+++ ising/__init__.py
@@ -5,6 +5,7 @@ Author: DKarandikar
 '''
 
 import numpy
+import time
 import matplotlib.pyplot as plt
 
 
@@ -21,8 +22,9 @@ def delta_e(lattice, site):
     return energy
 
 
-def mcmove(lattice, beta, move_n):
-    '''Flip a spin'''
+def mcmove(lattice, beta, move_n, exponentials):
+    '''Flip a spin if the energy change is beneficial'''
+
     for _ in range(move_n):
         pos_x = numpy.random.randint(0, lattice.shape[0])
         pos_y = numpy.random.randint(0, lattice.shape[1])
@@ -31,7 +33,7 @@ def mcmove(lattice, beta, move_n):
 
         if new_e <= 0:
             lattice[pos_x, pos_y] *= -1
-        elif numpy.exp(-1 * new_e * beta) > numpy.random.rand():
+        elif exponentials[new_e] > numpy.random.rand():
             lattice[pos_x, pos_y] *= -1
 
     return lattice
@@ -42,103 +44,92 @@ def mcsetup(grid_size):
     lattice = 2 * numpy.random.randint(2, size=(grid_size, grid_size)) - 1
     return lattice
 
-def calc_energy(lattice):
+def calc_energy(lattice, grid_size):
     '''Calculates the average energy of the system'''
     result = 0
-    size_x = lattice.shape[0]
-    size_y = lattice.shape[1]
     for i in range(lattice.shape[0]):
         for j in range(lattice.shape[1]):
-            site_e = lattice[i, j] * -1  * (lattice[(i-1)%size_x, j] \
-                                   + lattice[(i+1)%size_x, j] \
-                                   + lattice[i, (j-1)%size_y] \
-                                   + lattice[i, (j+1)%size_y])
+            site_e = lattice[i, j] * -1  * (lattice[(i-1)%grid_size, j] \
+                                   + lattice[(i+1)%grid_size, j] \
+                                   + lattice[i, (j-1)%grid_size] \
+                                   + lattice[i, (j+1)%grid_size])
             result += site_e
 
-    return result/(4*size_x*size_y)
+    return result/(4)
+
+def calc_magnet(lattice):
+    magnet = numpy.sum(lattice)
+    return magnet
 
 
-def mcrun(size, temp, n_0, n_max, move_n):
+def mcrun(size, temp, n_0, n_max, move_n, grid_size):
     '''Runs an entire simulation for a given temperature and returns the energy average'''
     lattice = mcsetup(size)
+
+    exponentials = {4 : numpy.exp(-1*4*(1/temp)), 8 : numpy.exp(-1*8*(1/temp))}
+
     for _ in range(n_0):
-        lattice = mcmove(lattice, 1/temp, move_n)
+        lattice = mcmove(lattice, 1/temp, move_n, exponentials)
 
     energy = 0
+    magnet = 0
 
     for _ in range(n_max):
-        lattice = mcmove(lattice, 1/temp, move_n)
-        energy += calc_energy(lattice)
+        lattice = mcmove(lattice, 1/temp, move_n, exponentials)
+        energy += calc_energy(lattice, grid_size)
+        magnet += calc_magnet(lattice)
 
-    return energy/n_max
+    return (energy/n_max, magnet/n_max)
 
-def e_t_graph(n_0, n_max, move_n, temp_steps, temp_range):
+def e_t_graph(n_0, n_max, move_n, temp_steps, temp_range, testing=False):
     '''Runs multiple temp simulations and then produces an energy-temperature graph'''
 
     grid_size = 16
     temperatures = numpy.linspace(temp_range[0], temp_range[1], temp_steps)
+
     energies = numpy.zeros(temp_steps)
+    magnetizations = numpy.zeros(temp_steps)
 
     for k, temp in enumerate(temperatures):
-        energies[k] = mcrun(grid_size, temp, n_0, n_max, move_n)
+        values = mcrun(grid_size, temp, n_0, n_max, move_n, grid_size)
+        energies[k] = values[0]/(grid_size**2)
+        magnetizations[k] = abs(values[1]/(grid_size**2))
         print(k/temp_steps)
 
-    #figure = plt.figure(figsize=(18, 10), dpi=80, facecolor='w', edgecolor='k')
+    figure = plt.figure(figsize=(18, 10), dpi=80, facecolor='w', edgecolor='k')
 
+    subplot = figure.add_subplot(2, 1, 1)
     plt.plot(temperatures, energies, 'o', color="#A60628", label=' Energy')
-    plt.xlabel("Temperature (T)", fontsize=20)
+    plt.xlabel("Temperature ", fontsize=20)
     plt.ylabel("Energy ", fontsize=20)
-    plt.show()
+
+    subplot = figure.add_subplot(2, 1, 2)
+    plt.plot(temperatures, magnetizations, 'o', color="#A60628", label=' Magnetization')
+    plt.xlabel("Temperature ", fontsize=20)
+    plt.ylabel("Magnetization ", fontsize=20)
+
+    if not testing:
+        plt.show()
 
     print("done")
 
-def test():
-    e_t_graph(100, 100, 50, 50, (1, 4))
+def test(testing=False):
+    '''Runs a quick test to see that e_t is working'''
+    e_t_graph(100, 100, 50, 50, (1, 4), testing)
 
-def configplot():
-    '''Plots multiple subplots'''
-
-    n = 16
-    f= plt.figure(figsize=(18, 10), dpi=80, facecolor='w', edgecolor='k')
-
-    lattice = mcsetup(n)
-    for i in range(5001):
-        lattice = mcmove(lattice, 1/3)
-        if i == 1:
-            subplot(f, lattice, i, n, 2)
-        if i == 10:
-            subplot(f, lattice, i, n, 3)
-        if i == 100:
-            subplot(f, lattice, i, n, 4)
-        if i == 100:
-            subplot(f, lattice, i, n, 5)
-        if i == 5000:
-            subplot(f, lattice, i, n, 6)
-        #print(i)
-
-    plt.show()
-
-def subplot(f, lattice, i, n, n_):
-    '''Appends a subplot to f'''
-    X, Y = numpy.meshgrid(range(n), range(n))
-    sp = f.add_subplot(3, 3, n_ )
-    plt.pcolormesh(X, Y, lattice)
+def time_test():
+    '''Performs multiple tests and averages the time'''
+    times = 0
+    number_tests = 10
+    for _ in range(number_tests):
+        start = time.time()
+        test(True)
+        times += (time.time() - start)
+    print('Took ' + '%.2f' % (times/number_tests) + ' seconds')
 
 
-
-### Main Section ###
-
-N_0 = 400
-N_MAX = 400
-MOVE_N = 300
-T_STEPS = 100
-
-#testing = input("Type y for test and n for full")
-
-#if testing == "n":
-#    e_t_graph(N_0, N_MAX, MOVE_N, T_STEPS, (1, 4))
-#elif testing == "y":
-#    e_t_graph(100, 100, 50, 50, (1, 4))
+def standard():
+    '''Runs a standard density simulation'''
+    e_t_graph(400, 400, 300, 100, (1, 4))
 
 
-#configplot()
